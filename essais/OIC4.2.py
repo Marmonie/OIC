8933ce818a8762021daa92046f440424c0d23114
Orianne Delmarre <orianne.delmarre@gmail.com>
2024-03-15 19:05:01 +0100

    ajout exo 4.2 et image

---
+++ essais/OIC4.2.py
@@ -0,0 +1,19 @@
+"""
+Nom ......... : OIC4.2.py
+Rôle ........ : exercice 4.2 du cours OIC pour découvrir streamlit
+Auteur ...... : Orianne Delmarre
+Version ..... : V0.1 du 15/03/2024
+Licence ..... : réalisé dans le cadre du cours 'Outils informatiques collaboratifs' de M Kislin-Duval
+    de la Licence 1 informatique de l'IED Paris 8
+Version de python3 utilisée : 10.6
+"""
+
+import streamlit as st
+from PIL import Image
+
+img = Image.open('P7070925.JPG')
+st.image(img, caption='Marmonie voyage')
+
+exif = img.getexif()
+exif.get_ifd(34665)[36868] = "2023:01:01 00:00:00"
+img.save('test.jpg', exif = exif)
