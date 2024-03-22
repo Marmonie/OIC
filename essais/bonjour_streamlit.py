f5db3c40f3b7985e1ab98c55c7497a4e5ad26286
Orianne Delmarre <orianne.delmarre@gmail.com>
2024-03-22 18:48:23 +0100

    ajout image test + bonjour_streamlit augmenté

--- essais/bonjour_streamlit.py
+++ essais/bonjour_streamlit.py
@@ -9,6 +9,23 @@ Version de python3 utilisée : 10.6
 """
 
 import streamlit as st
-prenom = st.text_input('Quel est votre prénom ?')
+from PIL import Image
+
+prenom = st.text_input('st.text_input : Quel est votre prénom ?')
 message = st.text_input('Quel est votre message ?')
-st.write(message, " ", prenom)
+st.write('st.write : ', message, " ", prenom)
+
+
+st.header('st.header')
+
+if st.button('st.button : Say hello'):
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
+
+img = Image.open('P7070925.JPG')
+st.image(img, caption='Marmonie voyage')
+
+exif = img.getexif()
+exif.get_ifd(34665)[36868] = "2023:01:01 00:00:00"
+img.save('test.jpg', exif = exif)
