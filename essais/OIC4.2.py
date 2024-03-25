c38461eb623ef5c4bcf4cce1eca11d0a294bcd15
Orianne Delmarre <orianne.delmarre@gmail.com>
2024-03-25 19:34:52 +0100

    formulaire manuel et test affichage données exif

--- essais/OIC4.2.py
+++ essais/OIC4.2.py
@@ -6,20 +6,56 @@ Version ..... : V0.1 du 15/03/2024
 Licence ..... : réalisé dans le cadre du cours 'Outils informatiques collaboratifs' de M Kislin-Duval
     de la Licence 1 informatique de l'IED Paris 8
 Version de python3 utilisée : 10.6
+Usage : Pour exécuter : streamlit run https://github.com/Marmonie/OIC/blob/main/essais/OIC4.2.py
 """
 
 import streamlit as st
-from PIL import Image
+# https://pillow.readthedocs.io/en/stable/reference/Image.html
+from PIL import Image, ExifTags
 import io
 import requests
 
+# Affichage image légendée à partir du repo github
+# https://pillow.readthedocs.io/en/stable/PIL.html#PIL.UnidentifiedImageError
 url_img = ('https://github.com/Marmonie/OIC/blob/main/essais/P7070925.jpg?raw=true')
 r_img = requests.get(url_img)
 if r_img.status_code == 200:
     img = Image.open(io.BytesIO(r_img.content))
+
 #img = Image.open(r_img.raw)
 st.image(img, caption='Marmonie voyage')
 
+# Formulaire st.form
+with st.form("form : modif des données exif"):
+    # Première ligne
+    auteur = st.columns([1, 3])
+    auteur[0].subheader('Auteur')
+    aut = auteur[1].text_input('Qui a pris cette photo ?')
+
+    # Deuxième ligne
+    date = st.columns([2, 3, 3])
+    date[0].subheader('Date')
+    jour = date[1].date_input('jour')
+    heure = date[2].time_input('heure')
+
+    # Troisième ligne
+    loc = st.columns([1, 1, 1, 1])
+    loc[0].subheader('Localisation')
+    lat = loc[1].text_input('latitude')
+    lon = loc[2].text_input('longitude')
+    alt = loc[3].text_input('altitude')
+
+    st.form_submit_button('Enregistrer la photo avec les nouvelles données')
+
+
 exif = img.getexif()
-exif.get_ifd(34665)[36868] = "2023:01:01 00:00:00"
-img.save('test.jpg', exif = exif)
+# exif.get_ifd(34665)[36868] = "2023:01:01 00:00:00"
+# img.save('test.jpg', exif = exif)
+
+# liste des données exif disponibles (code, libellé et valeur)
+dic_exif = {}
+for k, v in exif.items():
+    print('tag', k, 'label', ExifTags.TAGS.get(k, k), 'value', v)
+    toto = type(k)
+
+print(toto)
--- essais/bonjour_streamlit.py
+++ essais/bonjour_streamlit.py
@@ -11,21 +11,40 @@ Version de python3 utilisée : 10.6
 import streamlit as st
 from PIL import Image
 
-prenom = st.text_input('st.text_input : Quel est votre prénom ?')
+prenom = st.text_input('text_input : Quel est votre prénom ?')
 message = st.text_input('Quel est votre message ?')
-st.write('st.write : ', message, " ", prenom)
+st.write('write : ', message, " ", prenom)
 
 
-st.header('st.header')
+st.header('header')
 
-if st.button('st.button : Say hello'):
+if st.button('button : Say hello'):
      st.write('Why hello there')
 else:
      st.write('Goodbye')
 
-img = Image.open('P7070925.JPG')
-st.image(img, caption='Marmonie voyage')
+img = Image.open('P7070925.jpg')
+st.image(img, caption='image : Marmonie voyage')
+
+# Formulaire st.form
+with st.form("form : modif des données exif"):
+    # Première ligne
+    titres = st.columns([1, 1, 1])
+    titres[0].subheader('auteur')
+    titres[1].subheader('date')
+    titres[2].subheader('lieu')
+
+    # Deuxième ligne
+    ligne1 = st.columns([1, 1, 1])
+    auteur = ligne1[0].text_input('Qui sera l’auteur de cette photo ?')
+    date = ligne1[1].date_input('Quel jour voulez-vous la prendre ?')
+    coord_gps = ligne1[2].text_input('Où ça ?')
+
+    st.form_submit_button('form_submit_button : enregistrer la photo avec les nouvelles données')
 
 exif = img.getexif()
+print('exif = ', exif)
 exif.get_ifd(34665)[36868] = "2023:01:01 00:00:00"
 img.save('test.jpg', exif = exif)
+
+print(auteur, date, coord_gps)
