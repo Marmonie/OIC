e77099a49fc52c7b1c301b7a419966efac2aa121
Orianne Delmarre <orianne.delmarre@gmail.com>
2024-04-09 21:34:52 +0200

    suppression commentaires et bouts de code inutiles

--- essais/OIC4.2.py
+++ essais/OIC4.2.py
@@ -1,8 +1,8 @@
 """
 Nom ......... : OIC4.2.py
-Rôle ........ : exercice 4.2 du cours OIC pour découvrir streamlit
+Rôle ........ : développer une application streamlit avec affichage de photo, formulaire et cartes
 Auteur ...... : Orianne Delmarre
-Version ..... : V0.1 du 15/03/2024
+Version ..... : V1.1 du 9/4/2024
 Licence ..... : réalisé dans le cadre du cours 'Outils informatiques collaboratifs' de M Kislin-Duval
     de la Licence 1 informatique de l'IED Paris 8
 Version de python3 utilisée : 10.6
@@ -10,7 +10,6 @@ Usage : Pour exécuter : streamlit run https://github.com/Marmonie/OIC/blob/main
 """
 
 import streamlit as st
-# https://pillow.readthedocs.io/en/stable/reference/Image.html
 from PIL import Image, ExifTags
 import io
 import requests
@@ -77,12 +76,10 @@ def convert_coord(lat, lon):
 st.title("Carnet de voyage")
 
 # Première partie : insertion image légendée à partir du repo github
-# https://pillow.readthedocs.io/en/stable/PIL.html#PIL.UnidentifiedImageError
 r_img = requests.get(url_img)
 if r_img.status_code == 200:
     img = Image.open(io.BytesIO(r_img.content))
     st.image(img, caption='Marmonie voyage')
-    # img = Image.open(r_img.raw)
 else:
     st.write("Problème lors du chargement de l'image")
 
@@ -112,39 +109,14 @@ with st.form("formulaire auto de modif des données dispo"):
     img_lat = ligne_gps[1].number_input(f"Latitude (décimal)", min_value=-90.0, max_value=90.0, value=coord_Nice[0])
     img_lon = ligne_gps[2].number_input(f"Longitude (décimal)", min_value=-180.0, max_value=180.0, value=coord_Nice[1])
 
-    # Deuxième partie (suite) : conversion en format rational64u et édition des métadonnées GPS
+    # Deuxième partie, suite : conversion en format rational64u et édition des métadonnées GPS
     coord_exif = convert_coord(img_lat, img_lon)
     for k, v in coord_exif.items():
         exif.get_ifd(34853)[k] = v
 
     st.form_submit_button('Enregistrer la photo avec les nouvelles données')
 
-"""On pourrait intégrer quelques données supplémentaires (hauteur et largeur image par exemple) en utilisant les IFD"""
-# for ifd_id in ExifTags.IFD:
-#     try:
-#         ifd = exif.get_ifd(ifd_id)
-#         for k, v in ifd.items():
-#             label = ExifTags.TAGS.get(k, k)
-#             print(k, label, v)
-#     except KeyError:
-#         pass
-
-# Pour consulter les données de localisation
-# ifd = exif.get_ifd(ExifTags.IFD.GPSInfo)
-# for k, v in ifd.items():
-#     label = ExifTags.GPSTAGS.get(k, k)
-#     print(k, label, v)
-
 img.save('test.jpg', exif=exif)
-
-# liste des données exif disponibles (code, libellé et valeur)
-# for k, v in exif.items():
-#    print('tag', k, 'label', ExifTags.TAGS.get(k, k), 'value', v, 'type', type(v))
-
-# Pour connaître les données exif relatives à la position GPS
-# loc = ExifTags.GPSTAGS
-# print(loc)
-# {0: 'GPSVersionID', 1: 'GPSLatitudeRef', 2: 'GPSLatitude', 3: 'GPSLongitudeRef', 4: 'GPSLongitude', 5: 'GPSAltitudeRef', 6: 'GPSAltitude', 7: 'GPSTimeStamp', 8: 'GPSSatellites', 9: 'GPSStatus', 10: 'GPSMeasureMode', 11: 'GPSDOP', 12: 'GPSSpeedRef', 13: 'GPSSpeed', 14: 'GPSTrackRef', 15: 'GPSTrack', 16: 'GPSImgDirectionRef', 17: 'GPSImgDirection', 18: 'GPSMapDatum', 19: 'GPSDestLatitudeRef', 20: 'GPSDestLatitude', 21: 'GPSDestLongitudeRef', 22: 'GPSDestLongitude', 23: 'GPSDestBearingRef', 24: 'GPSDestBearing', 25: 'GPSDestDistanceRef', 26: 'GPSDestDistance', 27: 'GPSProcessingMethod', 28: 'GPSAreaInformation', 29: 'GPSDateStamp', 30: 'GPSDifferential', 31: 'GPSHPositioningError'}
 
 # Troisième partie : affichage des coordonnées GPS saisies dans une carte
 st.header("Dernière position connue de Marmonie…")
